48894c60c5611c746fb199888e323b62b565de6d
Ilya Dimov <ilyadimov98@gmail.com>
2025-04-23 08:41:10 +0000

    Update

--- qwen.sh
+++ qwen.sh
@@ -2,6 +2,6 @@
 # https://docs.vllm.ai/en/stable/getting_started/quickstart.html#openai-compatible-server
 # https://huggingface.co/microsoft/Phi-3-mini-128k-instruct/tree/main
 vllm serve \
-    microsoft/Phi-3-mini-128k-instruct \
+    Qwen/Qwen2.5-3B-Instruct \
     --max-model-len 4096 \
     --gpu-memory-utilization 0.5
