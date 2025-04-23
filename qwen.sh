b5a16163556403b687068265540e92e6a0d06b94
Ilya Dimov <ilyadimov98@gmail.com>
2025-04-23 22:07:38 +0000

    Finish

--- qwen.sh
+++ qwen.sh
@@ -1,6 +1,3 @@
-
-# https://docs.vllm.ai/en/stable/getting_started/quickstart.html#openai-compatible-server
-# https://huggingface.co/microsoft/Phi-3-mini-128k-instruct/tree/main
 vllm serve \
     Qwen/Qwen2.5-3B-Instruct \
     --max-model-len 4096 \
